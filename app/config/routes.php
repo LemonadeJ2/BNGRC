f0bd465f5783d06b32a56f778d789d047b5b7394
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-02-16 16:09:02 +0300

    login

--- app/config/routes.php
+++ app/config/routes.php
@@ -20,7 +20,7 @@ $router->group('', function(Router $router) use ($app) {
 		$app->render('login.php');
 	});
 
-	$router->post('/login_admin', function() use ($app) {
+	$router->post('/dashboard', function() use ($app) {
 		$adminController = new AdminController($app);
 		$adminController->loginAdmin();
 	});
