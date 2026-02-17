a569c76f002601592a2b38dbccfaf3e0ec50628d
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-02-17 11:12:21 +0300

    all_done

--- app/controllers/BesoinController.php
+++ app/controllers/BesoinController.php
@@ -62,53 +62,93 @@ class BesoinController
         // Récupérer les dons par ville
         $donsAttribues = $this->donController->listeDonsAttribuesParVille();
 
-        // Organiser les dons par ID de ville (plusieurs dons par ville possible)
+        // Récupérer tous les achats
+        $achatModel = new Achat(Flight::db());
+        $achats = $achatModel->getAchatsFiltrables();
+
+        // Organiser les achats par ville et par besoin
+        $achatsParVille = [];
+        foreach ($achats as $achat) {
+            $villeId = $achat['id_ville'];
+            $besoinId = $achat['id_besoin'];
+            if (!isset($achatsParVille[$villeId])) {
+                $achatsParVille[$villeId] = [];
+            }
+            if (!isset($achatsParVille[$villeId][$besoinId])) {
+                $achatsParVille[$villeId][$besoinId] = 0;
+            }
+            $achatsParVille[$villeId][$besoinId] += $achat['quantite'];
+        }
+
+        // Organiser les dons par ville et par besoin
         $donsParVille = [];
         foreach ($donsAttribues as $don) {
             $villeId = $don['id'];
+            $besoinNom = $don['besoin'];
             if (!isset($donsParVille[$villeId])) {
-                $donsParVille[$villeId] = [
-                    'dons' => [],
-                    'total_quantite' => 0
-                ];
+                $donsParVille[$villeId] = [];
             }
-            $donsParVille[$villeId]['dons'][] = $don;
-            $donsParVille[$villeId]['total_quantite'] += $don['quantite'];
+            if (!isset($donsParVille[$villeId][$besoinNom])) {
+                $donsParVille[$villeId][$besoinNom] = 0;
+            }
+            $donsParVille[$villeId][$besoinNom] += $don['quantite'];
         }
 
         // Construire le tableau $donParVille avec TOUTES les villes
         $donParVille = [];
+
         foreach ($toutesLesVilles as $ville) {
             $villeId = $ville['id'];
 
-            // Récupérer les besoins pour cette ville
-            $nb = $this->besoinModel->nbBesoinsParVille($villeId);
-            $listeBesoins = $this->besoinModel->getDetailBesoinVille($villeId);
+            // Récupérer TOUS les besoins pour cette ville
+            $tousBesoins = $this->besoinModel->getDetailBesoinVille($villeId);
 
-            // Préparer les informations de dons
-            if (isset($donsParVille[$villeId])) {
-                // La ville a des dons
-                foreach ($donsParVille[$villeId]['dons'] as $don) {
-                    $donParVille[] = [
-                        'id' => $villeId,
-                        'ville' => $ville['nom'],
-                        'nb_besoins' => $nb['nb_besoins'] ?? 0,
-                        'liste_besoins' => $listeBesoins,
-                        'besoin' => $don['besoin'],
-                        'quantite' => $don['quantite']
-                    ];
-                }
-            } else {
-                // La ville n'a pas de dons
-                $donParVille[] = [
-                    'id' => $villeId,
-                    'ville' => $ville['nom'],
-                    'nb_besoins' => $nb['nb_besoins'] ?? 0,
-                    'liste_besoins' => $listeBesoins,
-                    'besoin' => 'Aucun don',
-                    'quantite' => 0
+            // Construire la liste des besoins avec les quantités originales (pour le visuel)
+            // mais on va ajuster l'affichage en javascript plus tard si besoin
+            $besoinsListe = [];
+            foreach ($tousBesoins as $besoin) {
+                $besoinsListe[] = [
+                    'besoin' => $besoin['besoin'],
+                    'quantite_prevue' => $besoin['quantite_prevue']  // Garder la valeur originale pour le visuel
                 ];
             }
+
+            // Compter les besoins restants après achats (pour le badge)
+            $besoinsRestants = 0;
+            foreach ($tousBesoins as $besoin) {
+                $besoinNom = $besoin['besoin'];
+                $besoinId = $besoin['id_besoin'];
+                $quantite_originale = $besoin['quantite_prevue'] ?? 0;
+
+                $donRecu = $donsParVille[$villeId][$besoinNom] ?? 0;
+                $achatQuantite = $achatsParVille[$villeId][$besoinId] ?? 0;
+
+                if ($quantite_originale - $donRecu - $achatQuantite > 0) {
+                    $besoinsRestants++;
+                }
+            }
+
+            // Récupérer le premier don pour cette ville (pour la colonne Dons attribués)
+            $premierDon = null;
+            $quantiteDon = 0;
+            if (isset($donsParVille[$villeId])) {
+                foreach ($donsAttribues as $don) {
+                    if ($don['id'] == $villeId) {
+                        $premierDon = $don['besoin'];
+                        $quantiteDon = $don['quantite'];
+                        break;
+                    }
+                }
+            }
+
+            $donParVille[] = [
+                'id' => $villeId,
+                'ville' => $ville['nom'],
+                'nb_besoins' => $besoinsRestants,  // Ici le nombre de besoins ENCORE nécessaires
+                'liste_besoins' => $besoinsListe,  // Liste complète des besoins (pour le visuel)
+                'besoin' => $premierDon ?? 'Aucun don',
+                'quantite' => $quantiteDon ?? 0
+            ];
         }
 
         $admin = $_SESSION['admin'] ?? null;
@@ -117,7 +157,7 @@ class BesoinController
             'totalBesoins' => $totalBesoins,
             'totalDons' => $totalDons,
             'resteACombler' => $resteACombler,
-            'donParVille' => $donParVille,
+            'donParVille' => $donParVille,  // Structure identique à avant
             'admin' => $admin
         ]);
     }
